b4ccf51f0d30673ace1bb7e4c68b19dd108567b1
Zachary Zhao <zachary.m.zhao@gmail.com>
2018-08-06 16:22:41 -0700

    Implemented custom team number and event code functionality

--- dist/app.js
+++ dist/app.js
@@ -8,11 +8,8 @@ var apiKey = 'exJ8N6aK7Y5Fh8t2l5NDLJXaLaamWqI4ZjggCGvxDrbFiCca5Gn1kDj4FoYLypAb';
 //var apiUrl = myVars.apiUrl;
 //var apiKey = myVars.apiKey;
 
-console.log("apiUrl = " + apiUrl)
-console.log("apiKey = " + apiKey)
-
-var teamNum = '1758'
-var eventCode = 'scmb'
+//console.log("apiUrl = " + apiUrl)
+//console.log("apiKey = " + apiKey)
 
 function buildUrl (team, event) {
     return apiUrl + 'team/frc' + team + '/event/2017' + event + '/matches/simple' 
@@ -26,17 +23,51 @@ const vm = new Vue({
         categories: [
             'Match #', 'Red 1', 'Red 2', 'Red 3', 'Red Score', 'Blue Score', 'Blue 1', 'Blue 2', 'Blue 3'
         ],
-        matches: []
+        matches: [],
+        visible: false,
+        teamNum: '1758',
+        eventCode: 'scmb'
     },
     methods: {
         setParams: function (event) {
+            this.visible = true
+            this.teamNum = document.getElementById("teamNum").value;
+            this.eventCode = document.getElementById("eventCode").value;
+            console.log("teamNum: " + this.teamNum);
+            console.log("eventCode: " + this.eventCode);
 
-        },
-        test: function (event) {
-            alert('Test')
-        } 
+            this.matches = [];
+            
+            axios.get(buildUrl(this.teamNum, this.eventCode)).then(result => {
+                console.log(result.data); 
+                
+                var match;
+                for (match in result.data) {
+                    if (result.data[match].comp_level == "qm") {
+                        this.matches.push({
+                            "matchNum": result.data[match].match_number, 
+                            "red1": result.data[match].alliances.red.team_keys[0].substring(3),
+                            "red2": result.data[match].alliances.red.team_keys[1].substring(3),
+                            "red3": result.data[match].alliances.red.team_keys[2].substring(3),
+                            "redScore": result.data[match].alliances.red.score,
+                            "blueScore": result.data[match].alliances.blue.score,
+                            "blue1": result.data[match].alliances.blue.team_keys[0].substring(3),
+                            "blue2": result.data[match].alliances.blue.team_keys[1].substring(3),
+                            "blue3": result.data[match].alliances.blue.team_keys[2].substring(3)
+                        })
+                    }
+                }
+                
+                this.matches.sort(function(a, b) {
+                    return parseFloat(a.matchNum) - parseFloat(b.matchNum);
+                });
+    
+                console.log(this.matches);
+            })
+        }
     },
     mounted() {
+        /*
         axios.get(buildUrl(teamNum, eventCode)).then(result => {
             console.log(result.data); 
             
@@ -63,5 +94,6 @@ const vm = new Vue({
 
             console.log(this.matches);
         })
+        */
     }
 });
