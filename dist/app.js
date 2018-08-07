74b1d6908be27a9e8c85dd76a8920499c4e34173
Zachary Zhao <zachary.m.zhao@gmail.com>
2018-08-07 10:11:08 -0700

    Added custom year parameter

--- dist/app.js
+++ dist/app.js
@@ -11,8 +11,8 @@ var apiKey = 'exJ8N6aK7Y5Fh8t2l5NDLJXaLaamWqI4ZjggCGvxDrbFiCca5Gn1kDj4FoYLypAb';
 //console.log("apiUrl = " + apiUrl)
 //console.log("apiKey = " + apiKey)
 
-function buildUrl (team, event) {
-    return apiUrl + 'team/frc' + team + '/event/2017' + event + '/matches/simple' 
+function buildUrl (team, event, year) {
+    return apiUrl + 'team/frc' + team + '/event/' + year + event + '/matches/simple' 
 }
 
 axios.defaults.headers.common['X-TBA-Auth-Key'] = apiKey
@@ -24,21 +24,24 @@ const vm = new Vue({
             'Match #', 'Red 1', 'Red 2', 'Red 3', 'Red Score', 'Blue Score', 'Blue 1', 'Blue 2', 'Blue 3'
         ],
         matches: [],
+        events: [],
         visible: false,
-        teamNum: '1758',
-        eventCode: 'scmb'
+        teamNum: '',
+        eventCode: '',
+        year: ''
     },
     methods: {
-        setParams: function (event) {
+        createTable: function (event) {
             this.visible = true
             this.teamNum = document.getElementById("teamNum").value;
             this.eventCode = document.getElementById("eventCode").value;
+            this.year = document.getElementById("year").value;
             console.log("teamNum: " + this.teamNum);
             console.log("eventCode: " + this.eventCode);
 
             this.matches = [];
             
-            axios.get(buildUrl(this.teamNum, this.eventCode)).then(result => {
+            axios.get(buildUrl(this.teamNum, this.eventCode, this.year)).then(result => {
                 console.log(result.data); 
                 
                 var match;
@@ -62,38 +65,23 @@ const vm = new Vue({
                     return parseFloat(a.matchNum) - parseFloat(b.matchNum);
                 });
     
-                console.log(this.matches);
+                //console.log(this.matches);
             })
         }
     },
     mounted() {
-        /*
-        axios.get(buildUrl(teamNum, eventCode)).then(result => {
-            console.log(result.data); 
-            
-            var match;
-            for (match in result.data) {
-                if (result.data[match].comp_level == "qm") {
-                    this.matches.push({
-                        "matchNum": result.data[match].match_number, 
-                        "red1": result.data[match].alliances.red.team_keys[0].substring(3),
-                        "red2": result.data[match].alliances.red.team_keys[1].substring(3),
-                        "red3": result.data[match].alliances.red.team_keys[2].substring(3),
-                        "redScore": result.data[match].alliances.red.score,
-                        "blueScore": result.data[match].alliances.blue.score,
-                        "blue1": result.data[match].alliances.blue.team_keys[0].substring(3),
-                        "blue2": result.data[match].alliances.blue.team_keys[1].substring(3),
-                        "blue3": result.data[match].alliances.blue.team_keys[2].substring(3)
-                    })
-                }
-            }
-            
-            this.matches.sort(function(a, b) {
-                return parseFloat(a.matchNum) - parseFloat(b.matchNum);
-            });
+        axios.get(apiUrl + 'events/' + this.year + '/keys').then(result => {
 
-            console.log(this.matches);
+            // console.log(result.data)
+
+            var event;
+            for (event in result.data) {
+                this.events.push({
+                    "eventCode": result.data[event].substring(4)
+                })
+            }
+
+            // console.log(this.events)
         })
-        */
     }
 });
